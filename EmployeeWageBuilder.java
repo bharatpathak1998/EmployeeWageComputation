c95d581275c6bcc3c780dfb390d1c9efef406d6b
bharatpathak1998 <bharatpathak099@gmail.com>
2022-06-28 01:15:14 +0000

    initial commit

---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,36 @@
+package employeewage;
+
+public class EmployeeWageBuilder {
+
+	public static void main(String[] args) {
+		int PartTime=1;
+        int FullTime=2;
+        int wagePerHour=20;
+        int workingHrs=0;
+        int empWage=0;
+        
+        double empCheck =Math.floor(Math.random()*10)%3;
+        
+        if (empCheck==FullTime)
+        {   workingHrs=8;
+        	System.out.println("Employee is Present Full Time");
+        	
+        }
+        else
+        
+        if (empCheck==PartTime)
+        {   workingHrs=4;
+        	System.out.println("Employee is Present Part Time");
+        	
+        }
+        else	
+        {   workingHrs=0;
+        	System.out.println("Employee is Absent");
+        }
+        
+		empWage=wagePerHour*workingHrs;
+		System.out.println("Employee wage = "+empWage);
+			
+	}
+
+}
